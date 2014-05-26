742eb78b2f5c77c88d3fd8604dadd314049a83a2
Theara <yuom.theara@gmail.com>
2014-05-26 16:48:24 +0700

    use PHPExcel on Repayment Schedule

--- workbench/battambang/loan/src/controllers/RptScheduleController.php
+++ workbench/battambang/loan/src/controllers/RptScheduleController.php
@@ -10,7 +10,8 @@ namespace Battambang\Loan;
 
 
 use Battambang\Cpanel\BaseController;
-use Battambang\Cpanel\Libraries\Report;
+use Battambang\Cpanel\Company;
+use Battambang\Cpanel\Office;
 use DB;
 
 class RptScheduleController extends BaseController{
@@ -49,11 +50,79 @@ class RptScheduleController extends BaseController{
         //return \Excel::loadView('battambang/loan::rpt_schedule.pdf', $data)->export('xls');
 
         //if(count($data)>0){
-            \Report::setReportName('Schedule')->setDateFrom($data['view_at']);;
-            return \Report::make('rpt_schedule/source',$data,'repayment_schedules');
+//            \Report::setReportName('Schedule')->setDateFrom($data['view_at']);;
+//            return \Report::make('rpt_schedule/source',$data,'repayment_schedules');
         //}
 
+        $rptName='Repayment Schedule.xlsx';
+        $objReader = new \PHPExcel_Reader_Excel2007();
+        $objPHPExcel=$objReader->load(storage_path('packages/loan/'.$rptName));
+        $objWorkSheet=$objPHPExcel->getActiveSheet();
 
+        $companyName=Company::first()->kh_name;
+        $office=Office::find(\UserSession::read()->sub_branch);
+
+        $objWorkSheet->getCell('A1')->setValue($companyName);
+        $objWorkSheet->getCell('A2')->setValue('ការិយាល័យ៖ '.$office->kh_name);
+        $objWorkSheet->getCell('A3')->setValue('អាសយដ្ឋាន៖ '.$office->kh_address.', ទូរស័ព្ទ៖ '.$office->telephone);
+        $objWorkSheet->getCell('A5')->setValue('លេខកូដ៖ '.$data['dis']->ln_disburse_client_id.' ('.$data['dis']->account_type_code.')');
+        $objWorkSheet->getCell('A6')->setValue('ឈ្មោះ៖ '.$data['dis']->ln_client_kh_name);
+
+        $gender=($data['dis']->gender_code=='M'?'ប្រុស':'ស្រី');
+        $objWorkSheet->getCell('A7')->setValue('ភេទ៖ '.$gender);
+
+        $frequency=($data['dis']->repayment_frequency_type_name=='Weekly'?'សប្តាហ៍':'ខែ');
+        $objWorkSheet->getCell('A8')->setValue('ប្រភេទការសង៖ '.$frequency);
+
+        $objWorkSheet->getCell('A9')->setValue('រយៈពេលខ្ចី៖ '.$data['dis']->num_installment.' '. $frequency);
+        $objWorkSheet->getCell('A10')->setValue('អាសយដ្ឋាន៖ '.$data['dis']->address);
+
+        $objWorkSheet->getCell('D5')->setValue('រំលស់ការ៖ '.$data['dis']->installment_frequency.' '.$frequency.'ម្តង');
+        $objWorkSheet->getCell('D6')->setValue('រំលស់ដើម៖ '.$data['dis']->installment_principal_frequency.' '.$frequency.'ម្តង');
+        $objWorkSheet->getCell('D7')->setValue('រំលស់ដើម៖ '.$data['dis']->installment_principal_percentage.' %');
+        $objWorkSheet->getCell('D8')->setValue('អត្រាការប្រាក់៖ '.$data['dis']->interest_rate.' %');
+        $objWorkSheet->getCell('D9')->setValue('ចំនួនលើកនៃការខ្ចី៖ '.$data['dis']->cycle);
+
+        $objWorkSheet->getCell('F5')->setValue('កាលបរិច្ឆេទខ្ចី៖ '.date('d-m-Y',strtotime($data['dis']->ln_disburse_date)));
+        $objWorkSheet->getCell('F6')->setValue('លេខប័ណ្ណបើកប្រាក់៖ '.substr($data['dis']->voucher_id,-6));
+        if($data['dis']->cp_currency_code=='KHR'){
+            $currency='រៀល';
+        }elseif($data['dis']->cp_currency_code=='USD'){
+            $currency='ដុល្លារ';
+        }else{// THB
+            $currency='បាត';
+        }
+        $objWorkSheet->getCell('F7')->setValue('ចំនួនទឹកប្រាក់៖ '.number_format($data['dis']->amount,2,'.',',').' '.$currency);
+        $objWorkSheet->getCell('F8')->setValue('សោហ៊ុយសេវា៖ ');
+        $objWorkSheet->getCell('F9')->setValue('មន្រ្តីឥណទាន៖ '.$data['dis']->ln_staff_id.' | '.$data['dis']->ln_staff_kh_name);
+
+        // Content
+        $count=count($data['result']);
+        $objWorkSheet->insertNewRowBefore(14, $count);
+        $objWorkSheet->removeRow(13, 2);
+        foreach($data['result'] as $key=>$value){
+            $rowNum=13+$key;
+            $dueDate=\LookupValueList::getKhmerDay($value->due_date).' '.$value->due_date;
+            $objWorkSheet->getCell('A'.$rowNum)->setValue($key);
+            $objWorkSheet->getCell('B'.$rowNum)->setValue($dueDate);
+            $objWorkSheet->getCell('C'.$rowNum)->setValue($value->num_day);
+            $objWorkSheet->getCell('D'.$rowNum)->setValue($value->principal);
+            $objWorkSheet->getCell('E'.$rowNum)->setValue($value->interest);
+            $objWorkSheet->getCell('F'.$rowNum)->setValue('=D'.$rowNum.'+E'.$rowNum);
+            $objWorkSheet->getCell('G'.$rowNum)->setValue($value->balance);
+        }
+
+        // Add client name and disburse date to sign
+        $objWorkSheet->getCell('F'.(21-2+$count))->setValue($data['dis']->ln_client_kh_name);
+        $objWorkSheet->getCell('B'.(23-2+$count))->setValue('ថ្ងៃទី '.date('d-m-Y',strtotime($data['dis']->ln_disburse_date)));
+        $objWorkSheet->getCell('F'.(23-2+$count))->setValue('ថ្ងៃទី '.date('d-m-Y',strtotime($data['dis']->ln_disburse_date)));
+
+        // redirect output to client browser
+        header('Content-Type: application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="'.$rptName.'"');
+        header('Cache-Control: max-age=0');
+        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
+        $objWriter->save('php://output');
 
     }
 
