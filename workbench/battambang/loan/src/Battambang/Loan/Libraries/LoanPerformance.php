62e58b6f06615b31d4def67c82c5bcfecc7a7b9f
yuomtheara <yuomtheara@gmail.com>
2014-06-01 08:08:57 +0700

    (soknan) fix repayment on arrear error (3)

--- workbench/battambang/loan/src/Battambang/Loan/Libraries/LoanPerformance.php
+++ workbench/battambang/loan/src/Battambang/Loan/Libraries/LoanPerformance.php
@@ -774,15 +774,13 @@ WHERE ln_disburse_client.id = "'.$this->_disburse_client_id.'" ');
                     if($principal !=0){
                         if($key == 0){
                             if($this->_isDate($this->_arrears['last']['date'])){
-                                $tmp_p = $this->_arrears['last']['principal'] - $cPrin;
-                                $tmp_i = $this->_arrears['last']['interest'] - $cInt;
+                                $tmp_p = $this->_arrears['cur']['principal'] - $cPrin;
+                                $tmp_i = $this->_arrears['cur']['interest'] - $cInt;
 
-                                if($tmp_p > 0 and $tmp_i>0){
+                                $row->principal = $tmp_p;
+                                $row->interest = $tmp_i;
+                                if($tmp_i <=0){
                                     $row->principal = $tmp_p;
-                                    $row->interest = 0;
-                                }
-                                if($tmp_p>0 and $i<=0){
-                                    $row->principal = $tmp_p - $tmp_i;
                                     $row->interest = 0;
                                 }
                             }
