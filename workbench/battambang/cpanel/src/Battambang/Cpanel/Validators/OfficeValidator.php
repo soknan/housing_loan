7d0593ebbdbafc78795a3b59d7da8d846fd224ec
SOKNAN <soknanmean@gmail.com>
2014-11-27 19:10:10 +0700

    fix duplicate branch office

--- workbench/battambang/cpanel/src/Battambang/Cpanel/Validators/OfficeValidator.php
+++ workbench/battambang/cpanel/src/Battambang/Cpanel/Validators/OfficeValidator.php
@@ -7,10 +7,26 @@ class OfficeValidator extends \ValidatorAssistant
 
     protected function before()
     {
-        \Rule::add('kh_name')->required();
-        \Rule::add('kh_short_name')->required();
-        \Rule::add('en_name')->required();
-        \Rule::add('en_short_name')->required();
+        \Rule::add('kh_name')->required()->unique(
+            'cp_office',
+            'kh_name',
+            \Request::segment(4)
+        );
+        \Rule::add('kh_short_name')->required()->unique(
+            'cp_office',
+            'kh_short_name',
+            \Request::segment(4)
+        );
+        \Rule::add('en_name')->required()->unique(
+            'cp_office',
+            'en_name',
+            \Request::segment(4)
+        );
+        \Rule::add('en_short_name')->required()->unique(
+            'cp_office',
+            'en_short_name',
+            \Request::segment(4)
+        );
         \Rule::add('register_at')->required();
         \Rule::add('kh_address')->required();
         \Rule::add('en_address')->required();
