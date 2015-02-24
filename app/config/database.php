40c2a735efe3f760f308c053ca7cacc7e04a7443
Chum Phalkun <chum.phalkun@gmail.com>
2015-02-24 17:46:17 +0700

    Fix repayment schedule for Nikom,  footer and header

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'nikom',
+			'database'  => 'kra',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
